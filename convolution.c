c04cb7c3c122a5d503a7828546120a9917373948
Dave Paul Joseph <davepauljoseph@gmail.com>
2023-11-08 08:30:59 +0530

    Enabled user input for arrays

--- convolution.c
+++ convolution.c
@@ -3,13 +3,16 @@
 int* convolve(int *x,int x_len,int *h,int h_len,int N){
     int x_padded[N];
     int *h_padded=(int *)malloc(N * sizeof(int));;
+
+    //Pads x to length N
     for(int i=0;i<N;i++){
         x_padded[i]=(i<x_len)?x[i]:0;
     }
+    //Pads h to length N
     for(int i=0;i<N;i++){
         h_padded[i]=(i<h_len)?h[i]:0;
     }
-
+    //Creates the shifted sw
     int h_conv[N][N];
     for(int i=0;i<N;i++){
         for(int j=0;j<N;j++){
@@ -32,12 +35,31 @@ int* convolve(int *x,int x_len,int *h,int h_len,int N){
     return h_padded;
 }
 int main(){
-    int x[]={1,1,1};
-    int h[]={4,9,19,1,3};
+    int x_len, h_len;
+    // Input for array x
+    printf("Enter the size of array x: ");
+    scanf("%d", &x_len);
+    int x[x_len];
+    printf("Enter the elements of array x:\n");
+    for (int i = 0; i < x_len; i++) {
+        printf("Enter element %d: ", i + 1);
+        scanf("%d", &x[i]);
+    }
 
-    int x_len=sizeof(x) / sizeof(x[0]);
-    int h_len=sizeof(h) / sizeof(h[0]);
-    int N=7;
+    // Input for array h
+    printf("Enter the size of array h: ");
+    scanf("%d", &h_len);
+    int h[h_len];
+    printf("Enter the elements of array h:\n");
+    for (int i = 0; i < h_len; i++) {
+        printf("Enter element %d: ", i + 1);
+        scanf("%d", &h[i]);
+    }
+
+    //Input for N
+    int N;
+    printf("Enter N: ");
+    scanf("%d", &N);
 
     int *result=convolve(x,x_len,h,h_len,N);
     for(int i=0;i<N;i++){
